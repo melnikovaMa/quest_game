42ded01af97d7570736f4b262900995c42fe208b
Mary <150128082+melnikovaMa@users.noreply.github.com>
2025-11-14 19:23:00 +0300

    downloading the database folder
    downloading the database folder, which contains the database of bot-user interactions

---
+++ datebase/messages.py
@@ -0,0 +1,21 @@
+from datebase.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.sql import func
+
+
+class TelegramUser(Base):
+    tablename = 'messages'
+    id_message = Column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        autoincrement=True
+    )
+    text_message = Column(
+        String(),
+        nullable=True
+    )
+    author_telegram_id = Column(
+        Integer,
+        ForeignKey('telegramusers.telegram_id')
+    )
---
+++ datebase/tg_users.py
@@ -0,0 +1,25 @@
+from datebase.base_model import Base
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+
+
+class TelegramUser(Base):
+    tablename = 'telegramusers'
+    telegram_id = Column(
+        Integer,
+        primary_key=True,
+        unique=True
+    )
+    username = Column(
+        String(),
+        nullable=True
+    )
+    first_name = Column(
+        String(),
+        nullable=True
+    )
+    time_add = Column(
+        DateTime,
+        server_default=func.now()
+    )
